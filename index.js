f275e293047ccaebd751a3d9ae764275c5f89cda
0xAnon <utkarshshail005@protonmail.com>
2023-11-24 17:47:39 +0530

    Google login added

--- index.js
+++ index.js
@@ -2,18 +2,25 @@ const express = require('express');
 const session = require('express-session');
 const passport = require('passport');
 const mongoose = require('mongoose')
-require('./auth');
+const cors = require('cors'); 
+const authRoute = require("./Routes/auth");
+require('./passport');
+require('dotenv').config()
 
 const app = express();
 
-function isLoggedIn(req, res, next) {
-  req.user ? next() : res.sendStatus(401);
-}
+app.use(
+	cors({
+		origin: process.env.CLIENT_URL,
+		methods: "GET,POST,PUT,DELETE",
+		credentials: true,
+	})
+);
 
 mongoose.connect(
     process.env.MONGO_URL,
     { useNewUrlParser: true, useUnifiedTopology: true }
-  );
+);
   
 const db = mongoose.connection;
 
@@ -26,32 +33,7 @@ app.use(session({ secret: 'cats', resave: false, saveUninitialized: true , cooki
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.get('/', (req, res) => {
-  res.send('<a href="/auth/google">Authenticate with Google</a>');
-});
+app.use("/auth", authRoute);
 
-app.get('/auth/google',
-  passport.authenticate('google', { scope: ['profile'] }
-));
-
-app.get('/auth/google/callback', 
-  passport.authenticate('google', { failureRedirect: '/auth/google/failure'}),
-  function(req, res) {
-    res.redirect('/protected');
-  });
-
-app.get('/protected', isLoggedIn, (req, res) => {
-  res.send(`Hello ${req.user.displayName}`);
-});
-
-app.get('/logout', (req, res) => {
-  req.logout();
-  req.session.destroy();
-  res.send('Goodbye!');
-});
-
-app.get('/auth/google/failure', (req, res) => {
-  res.send('Failed to authenticate..');
-});
 
 app.listen(8080, () => console.log('listening on port: 8080'));
